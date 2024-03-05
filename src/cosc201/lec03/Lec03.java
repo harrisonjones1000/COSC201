eb5fd44ea5f7a425d27523d7f0a99236eefc3e47
Michael Albert <michael.albert@otago.ac.nz>
2024-03-05 13:45:08 +1300

    Added both kinds of test to Lec03 code

--- src/cosc201/lec03/Lec03.java
+++ src/cosc201/lec03/Lec03.java
@@ -1,5 +1,8 @@
 package cosc201.lec03;
 import cosc201.utilities.Timer;
+
+import java.util.Random;
+
 import cosc201.unionfind.*;
 
 public class Lec03 {
@@ -9,22 +12,41 @@ public class Lec03 {
     UnionFind uf2 = new UF2();
     UnionFind uf3 = new UF3();
     int n = Integer.parseInt(args[0]);
-    test(uf1, n);
-    test(uf2, n);
-    test(uf3, n);
+    randomTest(uf1, n);
+    randomTest(uf2, n);
+    randomTest(uf3, n);
+    sequenceTest(uf1, n);
+    sequenceTest(uf2, n);
+    sequenceTest(uf3, n);
   }
   
-  public static void test(UnionFind u, int n) {
+  public static void randomTest(UnionFind u, int n) {
+    Random R = new Random();
+    int[] x = new int[n];
+    int[] y = new int[n];
+    for(int i = 0; i < n; i++) {
+      x[i] = R.nextInt(n);
+      y[i] = R.nextInt(n);
+    }
     Timer t = new Timer();
     t.start();
     u.make(n);
     for(int i = 0; i < n; i++) {
-      int x = (int)(Math.random() * n);
-      int y = (int)(Math.random() * n);
-      u.union(x,y);
+      u.union(x[i],y[i]);
     }
     t.stop();
-    System.out.println(u.name() + " " + t.getTimeInMs());
+    System.out.println(n + " random unions with " + u.name() + " in " + t.getTimeInMs() + "ms");
+  }
+
+  public static void sequenceTest(UnionFind u, int n) {
+    Timer t = new Timer();
+    t.start();
+    u.make(n);
+    for(int i = 0; i < n; i++) {
+      u.union(0, i);
+    }
+    t.stop();
+    System.out.println(n + " sequential unions with " + u.name() + " in " + t.getTimeInMs() + "ms");
   }
   
 }
